573ce646e424c59baecf48ed71c497db802d06a0
Syed Aman <sna274@nyu.edu>
2017-04-24 18:39:14 -0500

    return array of users on get-users GET request

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,12 @@
 var express = require('express')
 var router = express.Router()
 
+// store user database
+var users = require('../database/users.json')
+
 // define GET route that returns a list of users
 router.get('/get-users', (request, response) => {
-  response.status(200).send('list of users')
+  response.status(200).send(users.users)
 })
 
 // define POST route that creates a user
