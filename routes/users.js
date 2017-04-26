3ac5cba27a430e3265778e327705f6cc1ebedf71
Syed Aman <sna274@nyu.edu>
2017-04-26 14:06:29 -0500

    inject error handling middleware

--- routes/users.js
+++ routes/users.js
@@ -9,27 +9,36 @@ const config = require('../database/config.json')
 // store user database
 const users = require('../database/users.json')
 
+// create JWT to match user JWTs
 const jwtTest = jwt({
   secret: config.secret
 })
 
-// use route middleware
+// inject middleware
 router.use((request, response, next) => {
   // log requests
   console.log(request.method, request.url)
 
-  // continue HTTP request
   next()
 })
 
-// define GET route that returns a list of users
+// define protected GET route that returns a list of users
 router.get('/get-users', jwtTest, (request, response) => {
   response.status(200).send(users.users)
 })
 
-// define POST route that creates a user
+// define unprotected POST route that creates a user
 router.post('/create-user', (request, response) => {
   response.status(200).send('successfully created a user')
+})
+
+// inject error handling middleware
+router.use((error, request, response, next) => {
+  if (error.name === 'UnauthorizedError') {
+    response.status(401).send('invalid token')
+  }
+
+  next(error)
 })
 
 // export routes
